880dbe1fdebd2fba78a0da75b9a6ea9c13c2a52e
Ryuk-4 <vito.damaso@gmail.com>
2019-04-02 15:03:59 +0200

    Create new activity

---
+++ Lab1/appComplete/restaurant/src/main/java/it/polito/mad/appcomplete/DailyOfferActivity.java
@@ -0,0 +1,29 @@
+package it.polito.mad.appcomplete;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DailyOfferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_offer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
