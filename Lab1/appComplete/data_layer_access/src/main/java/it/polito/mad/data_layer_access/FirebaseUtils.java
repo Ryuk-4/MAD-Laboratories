d9fd32cbfe4bf682ad14e2cdc5f904787b924fbf
Ryuk-4 <vito.damaso@gmail.com>
2019-05-31 16:11:38 +0200

    update

--- Lab1/appComplete/data_layer_access/src/main/java/it/polito/mad/data_layer_access/FirebaseUtils.java
+++ Lab1/appComplete/data_layer_access/src/main/java/it/polito/mad/data_layer_access/FirebaseUtils.java
@@ -23,6 +23,7 @@ public class FirebaseUtils {
     public static DatabaseReference branchOrdersInPreparation;
     public static DatabaseReference branchOrdersReady;
     public static DatabaseReference branchDailyFood;
+    public static DatabaseReference branchStoricFood;
     public static DatabaseReference branchFavouriteFood;
     public static DatabaseReference branchCustomer;
     public static DatabaseReference branchRestaurantProfile;
@@ -60,6 +61,7 @@ public class FirebaseUtils {
         timeBranch = database.child("restaurants/" + Uid + "/Time");
         popularFoodBranch = database.child("restaurants/" + Uid + "/Food_Analytics");
         branchDailyFood = database.child("restaurants/" + Uid + "/Daily_Food");
+        branchStoricFood = database.child("restaurants/" + Uid + "/Storic_Food");
         branchFavouriteFood = database.child("restaurants/" + Uid + "/Favourites_Food/");
         branchRestaurantOrders = database.child("restaurants/" + Uid + "/Orders/");
         branchOrdersFlag = database.child("restaurants/" + Uid + "/Orders/IncomingReservationFlag");
