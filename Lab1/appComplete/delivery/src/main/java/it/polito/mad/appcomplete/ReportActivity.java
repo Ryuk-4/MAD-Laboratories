76a46fabc50356edd5f3dc73c9a704debde4d94e
Mohammadali Ghezelbashyan <sepidehyefanavari@gmail.com>
2019-05-31 16:16:21 +0200

    fixed menu

--- Lab1/appComplete/delivery/src/main/java/it/polito/mad/appcomplete/ReportActivity.java
+++ Lab1/appComplete/delivery/src/main/java/it/polito/mad/appcomplete/ReportActivity.java
@@ -1,18 +1,26 @@
 package it.polito.mad.appcomplete;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,6 +36,14 @@ public class ReportActivity extends AppCompatActivity implements
     TextView TextViewIncome;
     private DatabaseReference database;
     private SharedPreferences sharedpref, preferences;
+    private DatabaseReference branchOrders;
+    private boolean newOrders;
+
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private Menu mMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +67,30 @@ public class ReportActivity extends AppCompatActivity implements
 
         TextViewTotalDistance = findViewById(R.id.textViewTotalDistance);
         TextViewIncome = findViewById(R.id.TextViewIncome);
+
+        // For new incoming notification:
+        preferences = getSharedPreferences("loginState", Context.MODE_PRIVATE);
+        database = FirebaseDatabase.getInstance().getReference();
+        branchOrders = database.child("delivery/" +
+                preferences.getString("Uid", "") + "/Orders/IncomingReservationFlag");
+
+        branchOrders.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                newOrders = dataSnapshot.getValue(Boolean.class);
+                if(newOrders == true)
+                    Toast.makeText(ReportActivity.this, "You have a new Reservation.", Toast.LENGTH_LONG).show();
+
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+               // Log.w(TAG, "onCancelled: The read failed: " + databaseError.getMessage());
+            }
+        });
+
 
         initializeData();
 
@@ -97,8 +137,68 @@ public class ReportActivity extends AppCompatActivity implements
         return bd;
     }
 
+
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch (item.getItemId()) {
+            case R.id.edit_action:
+                //This action will happen when is clicked the edit button in the action bar
+                Intent intent = new Intent(this, ProfileEditActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.logoutButton:
+                //logout();
+                finish();
+                break;
+
+
+            case R.id.new_order_incoming:
+                branchOrders.setValue(false);
+                startActivity(new Intent(this, ReservationActivity.class));
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_profile) {
+            Intent intent = new Intent(this,ProfileActivity.class);
+            startActivity(intent);
+            finish();
+
+        }else if (id == R.id.nav_reservation) {
+            Intent intent = new Intent(this,ReservationActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+        else if (id == R.id.nav_dailyMenu) {
+            //Intent intent = new Intent(this, ReportActivity.class);
+            //Intent intent = new Intent(this, ReportActivity.class);
+
+           // startActivity(intent);
+            finish();
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_contactUs) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_report);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
 }
